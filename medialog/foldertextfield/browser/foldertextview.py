e13a66b2c39bd0524512559db19e5abc627940ac
Espen Moe-Nilssen <espen@medialog.no>
2013-04-03 16:51:52 +0200

    Adding an action to folders so you can have text field

---
+++ medialog/foldertextfield/browser/foldertextview.py
@@ -0,0 +1,54 @@
+from zope.interface import implements, Interface
+from Products.Five.utilities.marker import mark
+
+from Products.Five import BrowserView
+
+
+
+class IFolderTextView(Interface):
+    """
+    Folder view interface
+    """
+
+    def test():
+        """ test method"""
+    
+    
+class FolderTextView(BrowserView):
+    """
+    Folder view
+    """
+    implements(IFolderTextView)
+    
+    def test(self):
+        """
+        test method
+        """
+        dummy = _(u'a dummy string')
+
+        return {'dummy': dummy}
+
+
+
+class IFolderTextEnable(Interface):
+    """
+    Enable Folder textfield interface
+    """
+
+    def __call__():
+        """ enable text field method"""
+        
+
+class FolderTextEnable(BrowserView):
+    """
+    Enable Folder text field
+    """
+    implements(IFolderTextEnable)
+    
+    def __call__(self):
+        """
+        enable textfield
+        """
+        
+        mark(self, IFolderTextEnable)
+        
---
+++ medialog/foldertextfield/interfaces.py
@@ -0,0 +1,5 @@
+from zope.interface import Interface
+    
+class IFolderTextObject(Interface):
+    """Interface folder with text field"""
+    
